3a620c652c139962bb74774089b4ec4e1a8a58e9
matschi2 <79938194+matschi2@users.noreply.github.com>
2025-08-28 10:43:52 +0200

    feat: new properties etc

--- validate-schema.py
+++ validate-schema.py
@@ -1,13 +1,15 @@
-import jsonschema as jss
 import json
+from pathlib import Path
 
-with open("./schema/functions/Source.json") as schema_file:
-    schema = json.load(schema_file)
+import jsonschema as jss
 
+for file in Path("./schema").glob("**/*.json"):
+    with open(file) as schema_file:
+        schema = json.load(schema_file)
 
-try:
-    jss.Draft7Validator.check_schema(schema)
-    print("Schema is valid.")
+    try:
+        jss.Draft7Validator.check_schema(schema)
+        print("Schema is valid.")
 
-except jss.SchemaError as e:
-    print("Schema error: %s" % e)
+    except jss.SchemaError as e:
+        print("Schema error: %s" % e)
